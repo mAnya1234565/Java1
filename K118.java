00d2886b3b17a5ebb125cc1ca7eca25e2cc94140
mAnya1234565 <169143140+mAnya1234565@users.noreply.github.com>
2024-11-05 15:06:00 +0530

    Add files via upload

---
+++ K118.java
@@ -0,0 +1,24 @@
+
+
+
+
+public class K118 {
+
+	public static void main(String[] args) {
+            for(int i=1;i<11;i++){
+               System.out.println(2*i); 
+            }
+            for(int i=1;i<11;i++){
+                System.out.println(2*i+1); 
+                
+            }
+		
+                
+                
+                
+		
+		
+			
+		
+	}
+}
