a94a82e80fbdeb307c4ba92d95e4ed2badeb52f0
mAnya1234565 <169143140+mAnya1234565@users.noreply.github.com>
2024-11-12 14:27:46 +0530

    Add files via upload

---
+++ M8.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class M8 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File f1=new File("name.txt");
+		Scanner sc1=new Scanner(f1);
+		String name="";
+		Tools t1=new Tools();
+		
+		name=sc1.nextLine();
+		System.out.println(t1.reverse(name));
+		
+		name=sc1.nextLine();
+		System.out.println(t1.reverse(name));
+		
+		name=sc1.nextLine();
+		System.out.println(t1.reverse(name));
+
+	}
+
+}
+
